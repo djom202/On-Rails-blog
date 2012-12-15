9221dbb1ec5f2f525d97d6b842c268fe9f6fe155
Ing. Jonathan Olier <djom202@gmail.com>
2012-12-15 17:03:20 -0500

    Blog en On Rails
    Mi primera app en On Rails, es un blog que espero sea muy interactivo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Altiviaot::Application.config.secret_token = '5cc76d03dc22c8efeefebabe634d22fcfbd13917f4d00f9dd7d4d6cb4dc95c88fbe276ced1ee77b8c04f21668b8af218867ca643b5d9aa052f1fb2775f6a201e'
